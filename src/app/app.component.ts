924fe555f1b54ee8f793e43e1f585c03840744cf
Badineni Sai Vardhan <badinenisaivardhan@gmail.com>
2024-03-02 21:28:59 +0530

    To-do App

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'first-app';
+  title:string = 'To-Do App In Angular';
+  tasks:string[] = [];
+  completed:string[] = [];
+
+  addTask(taskName:string){
+    this.tasks.push(taskName)
+  }
+
+  deletTask(taskName:string){
+    let taskindex:number = this.tasks.indexOf(taskName);
+    if(taskindex != 0){
+      this.tasks.splice(taskindex, 1);
+    }
+  }
+
+  doneTask(taskName:string){
+    let taskindex:number = this.tasks.indexOf(taskName);
+    if(taskindex != 0){
+      this.tasks.splice(taskindex, 1);
+      this.completed.push(taskName)
+    }
+  }
+
 }
